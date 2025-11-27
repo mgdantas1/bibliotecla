e0b3b7277467afa3a3f605a67dc94d7405c44021
Cecilya <anacecilya17@gmail.com>
2025-11-27 16:39:38 -0300

    criar o html do index e da base, e fazer o app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-import database
+from flask import Flask, render_template
+from flask_login import LoginManager
+from sqlalchemy.orm import Session
+from database import engine
+from models.usuario import Users
+from controllers.auth import auth_bp
+
+app = Flask(__name__)
+app.secret_key = 'Segredo'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    with Session(bind=engine) as session:
+        usuario = session.query(Users).where(Users.id == user_id).first()
+    return usuario
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.register_blueprint(auth_bp)
