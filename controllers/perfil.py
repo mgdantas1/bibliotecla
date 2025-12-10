60baf4e8d5c5bf86e0aca63a79d30fece0ebf6d2
Cecilya <anacecilya17@gmail.com>
2025-12-10 20:21:12 -0300

    adicionar a rota de alterar senha

--- controllers/perfil.py
+++ controllers/perfil.py
@@ -2,7 +2,7 @@ from flask import redirect, request, render_template, url_for, Blueprint, flash
 from flask_login import logout_user
 from sqlalchemy.orm import Session
 from flask_login import login_required, current_user
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from models.usuario import Users
 from database import engine
 perfil_bp = Blueprint('perfil', __name__, template_folder='../templates/perfil')
@@ -38,6 +38,28 @@ def editar_perfil(user_id:int):
         return redirect(url_for('perfil.listar_perfil', user_id=user_id))
 
     return render_template('perfil/editar.html', user=user)
+
+@perfil_bp.route('/alterar_senha', methods=['GET', 'POST'])
+@login_required
+def alterar_senha():
+    with Session(bind=engine) as db:
+        user = db.get(Users, current_user.id)
+    if request.method == 'POST':
+        senha = request.form['senha']
+        senha_ant = request.form['senha_ant']
+        if check_password_hash(user.senha, senha):
+            flash('Sua senha precisa ser diferente', category='error')
+            return redirect(url_for('perfil.alterar_senha'))
+        if not check_password_hash(user.senha, senha_ant):
+            flash('Insira a senha correta!', category='error')
+            return redirect(url_for('perfil.alterar_senha'))
+        user.senha = generate_password_hash(senha)
+        with Session(bind=engine) as db:
+            db.commit()
+        flash('Senha modificada com sucesso!', category='success')
+        return redirect(url_for('perfil.listar_perfil', user_id=user.id))
+    
+    return render_template('alterar_senha.html', user=user)
 
 
 @perfil_bp.route('/deletar_usuario', methods=['GET'])
